7ca8938deda2694a7f55b4d7fe51f25da208d4ab
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-09-29 17:07:07 +0300

    Инициализация

---
+++ task_27/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+n=Math.Abs(n);
+
+int summa=0;
+int ostatok=0;
+int result=0;
+int n1=n;
+
+int i=0;
+int count=i;
+
+while (n<10)
+{ 
+    if (count<3)
+    {
+        Console.WriteLine("Вы издеваетесь? Введено однозначное число, введите другое! ");
+        n=Convert.ToInt32(Console.ReadLine());
+        count=count+i;
+        i++;
+    }
+    else
+    {
+        Console.WriteLine("Вы иcчерпали количество попыток ");  
+        break;
+    }  
+    n1=n;
+}
+
+if (n==10)
+{
+    Console.WriteLine("Сумма цифр в числе 10 = 1"); 
+}
+
+while (n1>10)// 123 //12
+{
+    {
+        ostatok = n1 % 10;//3  //2
+        summa = summa + ostatok;//0+3 //3+2
+        n1=n1/10;//123:10=12 //12:10=1
+        i++;
+    }
+    if (n1<10)
+    {   
+        result=summa+n1;
+        Console.WriteLine($"Сумма цифр во введенном числе {n} = " + result);
+    }
+}
+
---
+++ task_29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void OneArray(int[] array)
+ {
+     int length = array.Length;
+     int i = 0;
+     while (i < length)
+     {
+         Console.WriteLine($"Введите элемент массива [{i}]:");
+         int n = Convert.ToInt32(Console.ReadLine());
+         array[i] = n;
+         i++;
+     }
+ }
+ void PrintArray(int[] arr)
+ {
+     int count = arr.Length;
+     int i = 0;
+     while (i < count)
+     {
+         Console.Write(arr[i] + " ");
+         i++;
+     }
+ }
+ int[] array = new int[8];
+ int count = array.Length;
+ OneArray(array);
+ PrintArray(array);
+
